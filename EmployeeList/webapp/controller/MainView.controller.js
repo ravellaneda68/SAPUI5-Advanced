39f468f05b28c539b6355f02d41ab3d94a5f8e22
ravellaneda68 <ramiro.avellaneda@gmail.com>
2021-06-12 22:42:19 +0000

    First commit en 04_Elemento_controlador_UI5

--- EmployeeList/webapp/controller/MainView.controller.js
+++ EmployeeList/webapp/controller/MainView.controller.js
@@ -1,15 +1,32 @@
+//@ts-nocheck
+
 sap.ui.define([
-		"sap/ui/core/mvc/Controller"
-	],
+    "sap/ui/core/mvc/Controller"
+],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-	function (Controller) {
-		"use strict";
+    function (Controller) {
+        "use strict";
 
-		return Controller.extend("logaligroup.EmployeeList.controller.MainView", {
-			onInit: function () {
+        return Controller.extend("logaligroup.EmployeeList.controller.MainView", {
+            onInit: function () {
 
-			}
-		});
-	});
+            },
+
+            onValidate: function () {
+                var inputEmployee = this.byId("inputEmployee");
+                var valueEmployee = inputEmployee.getValue();
+
+                if (valueEmployee.length === 6) {
+                   // inputEmployee.setDescription("OK");
+                   this.getView().byId("labelCountry").setVisible(true);
+                   this.getView().byId("slCountry").setVisible(true);
+                } else {
+                  //  inputEmployee.setDescription("NOT OK");
+                  this.getView().byId("labelCountry").setVisible(false);
+                  this.getView().byId("slCountry").setVisible(false);
+                };
+            }
+        });
+    });
